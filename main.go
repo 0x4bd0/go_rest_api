e1f7668eedc65bb8e3ff3a0e87040594286d2661
oxi <abdoumjr@gmail.com>
2022-06-16 00:07:52 +0100

    init structs

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Stack string
+
+const (
+	BackEnd Stack = "BackEnd"
+	FrontEnd        = "FrontEnd" 
+	Mobile        = "Mobile"
+	FullStack        = "FullStack"
+)
+
+type Team struct { 
+	Id int `json:"id"`
+	Name string `json:"name"`
+}
+
+type Person struct {
+	Name string `json:"name"`
+	Stack  Stack `json:"stack"`
+	Team	int `json:"team"`
+}
+
+main() {
+
+}
