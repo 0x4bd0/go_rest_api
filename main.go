7d97b2d5a43c7315a4ce422ae2f86e77c6d80225
oxi <abdoumjr@gmail.com>
2022-06-16 01:48:24 +0100

    GET

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,19 +17,21 @@ const (
 )
 
 type Person struct {
+	Id string `json:"id"`
 	Name string `json:"name"`
 	Stack  Stack `json:"stack"`
 }
 
 var persons = []Person{
-	{Name: "John", Stack: BackEnd},
-	{Name: "Doe", Stack: FrontEnd},
-	{Name: "Smith", Stack: FullStack},
-	{Name: "Mark", Stack: Mobile},
-	{Name: "Matt", Stack: Mobile},
-	{Name: "Rick", Stack: BackEnd},
-	{Name: "Morty", Stack: FrontEnd},
+	{Name: "John", Stack: BackEnd, Id : "1"},
+	{Name: "Doe", Stack: FrontEnd, Id : "2"},
+	{Name: "Smith", Stack: FullStack, Id : "3"},
+	{Name: "Mark", Stack: Mobile, Id : "4"},
+	{Name: "Matt", Stack: Mobile, Id : "5"},
+	{Name: "Rick", Stack: BackEnd, Id : "6"},
+	{Name: "Morty", Stack: FrontEnd, Id : "7"},
 }
+var person Person
 
 func GetPersons(c *gin.Context) { 
 	c.JSON(http.StatusOK, gin.H{
@@ -58,14 +61,44 @@ func CreatePerson(c *gin.Context) {
 		"data " : person,
 		"status" : http.StatusCreated,
 	})
-
 }
 
+func GetPersonById(id string)(*Person, error) {
+
+	for _, person := range persons {
+		if(person.Id == id) {
+			return &person, nil
+		}
+	}
+
+	return nil, errors.New("Person not found")
+}
+
+func GetPerson(c *gin.Context) {
+	id := c.Param("id")
+
+	person, err := GetPersonById(id)
+	
+	if(err != nil) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": err.Error(),
+			"status" : http.StatusNotFound,
+	})
+	return
+}
+
+c.JSON(http.StatusOK, gin.H{
+	"message": "Fetched Data sucessfully",
+	"data " : person,
+})
+
+}
 func main() {
 
 	router := gin.Default()
 
 	router.GET("/persons", GetPersons)
+	router.GET("/persons/:id", GetPerson)
 	router.POST("/persons", CreatePerson)
 
 	router.Run("localhost:8088")
