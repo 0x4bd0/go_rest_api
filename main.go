757d17476ef385549160eabc7d75ba271e149d6a
oxi <abdoumjr@gmail.com>
2022-06-16 01:10:11 +0100

    :fire:

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,20 +15,59 @@ const (
 	FullStack        = "FullStack"
 )
 
-type Team struct { 
-	Id int `json:"id"`
-	Name string `json:"name"`
-}
-
 type Person struct {
 	Name string `json:"name"`
 	Stack  Stack `json:"stack"`
-	Team	int `json:"team"`
+}
+
+var persons = []Person{
+	{Name: "John", Stack: BackEnd},
+	{Name: "Doe", Stack: FrontEnd},
+	{Name: "Smith", Stack: FullStack},
+	{Name: "Mark", Stack: Mobile},
+	{Name: "Matt", Stack: Mobile},
+	{Name: "Rick", Stack: BackEnd},
+	{Name: "Morty", Stack: FrontEnd},
+}
+
+func GetPersons(c *gin.Context) { 
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Fetched Data sucessfully",
+		"data " : persons,
+		"status" : http.StatusOK,
+	})
+}
+
+func CreatePerson(c *gin.Context) {
+
+	var person Person
+
+	if err := c.ShouldBindJSON(&person); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid request",
+			"error": err.Error(),
+			"status" : http.StatusBadRequest,
+	})
+	return
+}
+
+	persons = append(persons, person)
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Created successfully",
+		"data " : person,
+		"status" : http.StatusCreated,
+	})
+
 }
 
 func main() {
 
 	router := gin.Default()
-	router.run("localhost:8088")
+
+	router.GET("/persons", GetPersons)
+	router.POST("/persons", CreatePerson)
+
+	router.Run("localhost:8088")
 
 }
